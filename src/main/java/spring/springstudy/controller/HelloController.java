01669fd17fbf8f6ba4fcb4f03b0a2d8588243385
Supreme-YS <dudtjr1225@gmail.com>
2021-05-21 17:08:05 +0900

    spring code update

--- src/main/java/spring/springstudy/controller/HelloController.java
+++ src/main/java/spring/springstudy/controller/HelloController.java
@@ -3,10 +3,13 @@ package spring.springstudy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HelloController {
+
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
@@ -14,6 +17,20 @@ public class HelloController {
         // resources -> templates -> hello.html 로 가서 렌더링 진행
         // 컨트롤러에서 리턴 값으로 문자를 반환하면 viewResolver가 화면을 찾아서 처리한다.
         // viewName 매핑 = 'resources:templates/'+{ViewName}+'.html'
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // Http 통신 body 부에 return 데이터를 직접 넣어주겠다는 뜻
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+        // 이후 페이지 소스보기를 하게 되면 html 코드들이 보이지 않고, return 값만 보이게 된다.
+        // hello-mvc와 차이점.
     }
 }
 
