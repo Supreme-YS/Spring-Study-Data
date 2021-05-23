cf0c47ee69c605b5bdf67c1e9855cf7bcdc0a461
Supreme-YS <dudtjr1225@gmail.com>
2021-05-23 19:28:21 +0900

    add look-up function
    * cmd + e = 최근 본 모델 리스트 조회
    * cmd + option + v = Optional 레퍼런스 자동 생성

--- src/main/java/spring/springstudy/controller/MemberController.java
+++ src/main/java/spring/springstudy/controller/MemberController.java
@@ -2,10 +2,13 @@ package spring.springstudy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import spring.springstudy.domain.Member;
 import spring.springstudy.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -28,9 +31,19 @@ public class MemberController {
         Member member = new Member();
         member.setName(form.getName());
 
+        // Add value test code
+        System.out.println("member : " + member.getName());
+
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers(); // cmd + option + v
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
 
