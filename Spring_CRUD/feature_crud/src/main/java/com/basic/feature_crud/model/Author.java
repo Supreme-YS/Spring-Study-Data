fa58500dd3e7e091c459024566fa42b3e4afebe8
Supreme-YS <dudtjr1225@gmail.com>
2021-07-05 19:02:43 +0900

    ORM doing :|

--- Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Author.java
+++ Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Author.java
@@ -1,18 +1,37 @@
 package com.basic.feature_crud.model;
 
+// 저자정보
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+// 저자와 도서는 1:N 관계, 한 저자는 여러개의 책을 쓸 수 있기 때문이다.
+
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "author")
+
+
+
+
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
+
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Book> books;
 }
--- Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Book.java
+++ Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Book.java
@@ -2,10 +2,16 @@ package com.basic.feature_crud.model;
 
 // 책 정보
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+
+
+// 저자와 도서는 1:N 관계, 한 저자는 여러개의 책을 쓸 수 있기 때문이다.
+
+
 
 @Entity
 @Getter
@@ -17,4 +23,9 @@ public class Book {
     private Long id;
     private String name;
     private String isbn;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    @JsonManagedReference
+    private Author author;
 }
