f0568ba2a20870c878c66a917e14aa3160436e14
Supreme-YS <dudtjr1225@gmail.com>
2021-07-05 19:28:41 +0900

    ORM doing :|

--- Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Lend.java
+++ Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Lend.java
@@ -2,11 +2,14 @@ package com.basic.feature_crud.model;
 
 // 대출
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.Instant;
+
+// 책과 대출은 1:N 관계, 하나의 책을 여러명이 대여할 수 있다.
 
 @Entity
 @Getter
@@ -23,4 +26,8 @@ public class Lend {
     @Enumerated(EnumType.STRING)
     private LendStatus status;
 
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    @JsonManagedReference
+    private Book book;
 }
