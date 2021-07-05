7df28444275dedb8e444f51574f1d62dcd25e3b5
Supreme-YS <dudtjr1225@gmail.com>
2021-07-05 19:35:11 +0900

    ORD Done :)

--- Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Lend.java
+++ Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Lend.java
@@ -9,8 +9,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.Instant;
 
-// 책과 대출은 1:N 관계, 하나의 책을 여러명이 대여할 수 있다.
-
 @Entity
 @Getter
 @Setter
@@ -26,8 +24,15 @@ public class Lend {
     @Enumerated(EnumType.STRING)
     private LendStatus status;
 
+    // 책과 대출은 1:N 관계, 하나의 책을 여러명이 대여할 수 있다.
     @ManyToOne
     @JoinColumn(name = "book_id")
     @JsonManagedReference
     private Book book;
+
+    // 한 명의 회원이 도서 여러권을 대출할 수 있다. 회원과 대출은 1:N 관계
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    @JsonManagedReference
+    private Member member;
 }
--- Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Member.java
+++ Spring_CRUD/feature_crud/src/main/java/com/basic/feature_crud/model/Member.java
@@ -2,10 +2,12 @@ package com.basic.feature_crud.model;
 
 // 회원정보
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,4 +24,9 @@ public class Member {
     // 회원 상태를 알려주는 Enumerated
     @Enumerated(EnumType.STRING)
     private MemberStatus status;
+
+    // 한 명의 회원이 도서 여러권을 대출할 수 있다. 회원과 대출은 1:N 관계
+    @JsonBackReference
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Lend> lends;
 }
