77da1cdbfe56c3b3a32c0d05d6e5a21f56584705
Supreme-YS <dudtjr1225@gmail.com>
2021-07-13 00:25:40 +0900

    make DTO, DTO test code :)

--- src/main/java/com/supreme/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/supreme/book/springboot/web/dto/HelloResponseDto.java
@@ -12,4 +12,6 @@ import lombok.RequiredArgsConstructor;
 // @RequiredArgsConstructor 선언된 모든 final 필드가 포함된 생성자를 생성시켜준다.
 @RequiredArgsConstructor
 public class HelloResponseDto {
+    private final String name;
+    private final int amount;
 }
