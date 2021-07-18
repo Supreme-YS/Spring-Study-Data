8d3444a849f01a125df8c07581199aea405dca0a
Supreme-YS <dudtjr1225@gmail.com>
2021-07-18 17:41:15 +0900

    make domain Entity

--- src/main/java/com/supreme/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/supreme/book/springboot/domain/posts/Posts.java
@@ -1,11 +1,13 @@
 package com.supreme.book.springboot.domain.posts;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@NoArgsConstructor
+@Getter // 클래스 내 모든 필드의 Getter 메소드를 자동생성
+@NoArgsConstructor // 기본 생성자 자동추가 public Posts() {} 와 같은 효과
 @Entity // 테이블과 링크될 클래스임을 나타낸다.
 public class Posts {
     @Id // 해당 테이블 필드의 PK 필드를 나타낸다.
@@ -20,7 +22,7 @@ public class Posts {
 
     private String author;
 
-    @Builder
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
