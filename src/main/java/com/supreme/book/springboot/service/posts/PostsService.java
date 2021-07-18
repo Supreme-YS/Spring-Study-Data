98d5670a8e85f425ceee9813bca80776fe54798c
Supreme-YS <dudtjr1225@gmail.com>
2021-07-18 19:00:53 +0900

    update

---
+++ src/main/java/com/supreme/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.supreme.book.springboot.service.posts;
+
+import com.supreme.book.springboot.domain.posts.PostsRepository;
+import com.supreme.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/supreme/book/springboot/web/PostsApiController.java
@@ -0,0 +1,22 @@
+package com.supreme.book.springboot.web;
+
+
+import com.supreme.book.springboot.service.posts.PostsService;
+import com.supreme.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postsService.save(requestDto);
+    }
+}
