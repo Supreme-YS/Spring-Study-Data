dc6cf4622a889afecc4e84dc89f9731cab75d924
Supreme-YS <dudtjr1225@gmail.com>
2021-11-22 19:23:51 +0900

    Order Domain Test Implements

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -21,5 +21,6 @@ public class OrderApp {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
         System.out.println("order = " + order);
+        System.out.println("order.calculatePrice = " + order.calculatePrice());
     }
 }
