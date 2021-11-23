29b6fe3b0429168bf4fa238a9b5e375562258f1b
Supreme-YS <dudtjr1225@gmail.com>
2021-11-23 18:43:48 +0900

    Implement Test Code

--- core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -20,7 +20,7 @@ public class ApplicationContextExtendsFindTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
 
     @Test
-    @DisplayName("부모 타입으 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다.")
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다.")
     void findBeanByParentTypeDuplicate() {
         assertThrows(NoUniqueBeanDefinitionException.class,
                 () ->ac.getBean(DiscountPolicy.class));
