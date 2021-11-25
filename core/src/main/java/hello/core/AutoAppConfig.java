b9b494aa8ddef03dbef6d1cfcfef6903aa11977e
Supreme-YS <dudtjr1225@gmail.com>
2021-11-25 12:24:45 +0900

    Bean Conflict Exception

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,5 +15,16 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository") // 첫 글자가 소문자가 되니까!
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    /*
+    * The bean 'memoryMemberRepository', defined in class path resource [hello/core/AutoAppConfig.class], could not be registered. A bean with that name has already been defined in file [/Users/supreme-ys/study/spring/spring-inflearn/core/out/production/classes/hello/core/member/MemoryMemberRepository.class] and overriding is disabled.
+      Action:
+      Consider renaming one of the beans or enabling overriding by setting spring.main.allow-bean-definition-overriding=true
+    */
 
 }
