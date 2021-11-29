88964a8df2895602cb8856f858608aca8a52142d
Supreme-YS <dudtjr1225@gmail.com>
2021-11-29 13:56:38 +0900

    @Autowired vs @Qualifier vs @Primary

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,10 +2,12 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 // command + shift + t 테스트 도구 자동 빌드
 @Component // @ComponentScan을 통한 Bean 등록
+@Primary // 2개 이상 빈 조회시 우선순위를 갖게 하는 것
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10; // 10% 할인
