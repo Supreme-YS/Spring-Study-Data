224df59b6fe3d2bff09806688833c37318c81d78
Supreme-YS <dudtjr1225@gmail.com>
2021-12-17 21:38:47 +0900

    Update

--- spring-trading/src/main/java/com/ireland/trading/domain/Member.java
+++ spring-trading/src/main/java/com/ireland/trading/domain/Member.java
@@ -30,7 +30,7 @@ public class Member {
         email = request.getEmail();
         password = request.getPassword();
         name = request.getName();
-        role = Role; // 회원가입하는 사용자 권한 기본 USER (임시)
+        role = Role.USER; // 회원가입하는 사용자 권한 기본 USER (임시)
     }
 
     public void encryptPassword(PasswordEncoder passwordEncoder) {
