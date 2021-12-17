30b1c75364678ccc9f56eb69a37c2468aeca0ae7
Supreme-YS <dudtjr1225@gmail.com>
2021-12-17 21:37:55 +0900

    Update

---
+++ spring-board-basic/src/main/java/com/supreme/board/test/Book.java
@@ -0,0 +1,24 @@
+package com.supreme.board.test;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+@Getter
+@Setter
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String isbn;
+    private String title;
+
+    @ManyToOne
+    private BookStore bookStore;
+}
---
+++ spring-trading/src/main/java/com/ireland/trading/test/BookStore.java
@@ -0,0 +1,2 @@
+package com.ireland.trading.test;public class BookStore {
+}
