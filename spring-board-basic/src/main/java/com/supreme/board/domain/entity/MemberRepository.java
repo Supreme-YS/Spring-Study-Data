7d40a06f953ffbc908f29cb716f253d246ed005c
Supreme-YS <dudtjr1225@gmail.com>
2021-12-19 11:28:02 +0900

    Revise JWT Token

--- spring-board-basic/src/main/java/com/supreme/board/domain/entity/MemberRepository.java
+++ spring-board-basic/src/main/java/com/supreme/board/domain/entity/MemberRepository.java
@@ -7,5 +7,4 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where email = :email and password = :password")
     Member findMember(String email, String password);
-
 }
