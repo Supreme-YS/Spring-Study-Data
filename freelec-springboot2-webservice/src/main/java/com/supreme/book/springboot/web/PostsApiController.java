ebd4387de4e7adb9298bb9e326fd5736b31fd7ca
Supreme-YS <dudtjr1225@gmail.com>
2021-12-21 20:49:33 +0900

    Post Update Test Code

---
+++ freelec-springboot2-webservice/src/main/java/com/supreme/book/springboot/web/PostsApiController.java
@@ -0,0 +1,33 @@
+package com.supreme.book.springboot.web;
+
+
+import com.supreme.book.springboot.domain.posts.Posts;
+import com.supreme.book.springboot.service.posts.PostsService;
+import com.supreme.book.springboot.web.dto.PostsResponseDto;
+import com.supreme.book.springboot.web.dto.PostsSaveRequestDto;
+import com.supreme.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@RequiredArgsConstructor // PostsService의 생성자를 생성, final이 붙은 것.
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
