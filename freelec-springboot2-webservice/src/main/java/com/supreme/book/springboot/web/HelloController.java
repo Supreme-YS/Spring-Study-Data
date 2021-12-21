786e06b607b31161be30fa608f36dcfbe7570ce0
Supreme-YS <dudtjr1225@gmail.com>
2021-12-21 17:46:25 +0900

    Add Data-JPA dependency

--- freelec-springboot2-webservice/src/main/java/com/supreme/book/springboot/web/HelloController.java
+++ freelec-springboot2-webservice/src/main/java/com/supreme/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.supreme.book.springboot.web;
 
+import com.supreme.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
@@ -10,5 +12,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    // @RequestParam : "name"이란 이름으로 넘긴 파라미터를 메소드 파라미터(String name)에 저장
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
