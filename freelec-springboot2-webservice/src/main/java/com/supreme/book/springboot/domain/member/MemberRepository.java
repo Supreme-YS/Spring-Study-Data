9e02be6ec4dc5aa7bc2ca6fc79dfe678595455e5
Supreme-YS <dudtjr1225@gmail.com>
2021-12-22 13:43:35 +0900

    Developing Sign-up using by Spring Security

---
+++ freelec-springboot2-webservice/src/main/java/com/supreme/book/springboot/domain/member/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.supreme.book.springboot.domain.member;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MemberRepository extends CrudRepository<Member, Long> {
+
+}
