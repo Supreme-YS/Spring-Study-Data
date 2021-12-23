fd3db952e3704ea5ed4e7ab85e6766b79c44b31a
Supreme-YS <dudtjr1225@gmail.com>
2021-12-23 11:59:13 +0900

    Replace Code

--- freelec-springboot2-webservice/src/test/java/com/mbappe/book/springboot/web/IndexControllerTest.java
+++ freelec-springboot2-webservice/src/test/java/com/mbappe/book/springboot/web/IndexControllerTest.java
@@ -13,7 +13,6 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class IndexControllerTest {
-
     @Autowired
     private TestRestTemplate restTemplate;
 
--- freelec-springboot2-webservice/src/test/java/com/mbappe/book/springboot/web/PostsApiControllerTest.java
+++ freelec-springboot2-webservice/src/test/java/com/mbappe/book/springboot/web/PostsApiControllerTest.java
@@ -20,26 +20,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 public class PostsApiControllerTest {
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private PostsRepository postsRepository;
 
     @Autowired
     private WebApplicationContext context;
@@ -53,6 +46,13 @@ public class PostsApiControllerTest {
                 .apply(springSecurity())
                 .build();
     }
+
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Autowired
+    private PostsRepository postsRepository;
 
     @After
     public void tearDown() throws Exception {
