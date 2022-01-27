5a1b0e1e0d17ea0c877e44086ba5b3d4c5826e5b
Supreme-YS <dudtjr1225@gmail.com>
2022-01-27 19:05:23 +0900

    TODO, FIXME comment

--- spring-inflearn-core-remind/src/main/java/practice/spring/core/order/OrderServiceImpl.java
+++ spring-inflearn-core-remind/src/main/java/practice/spring/core/order/OrderServiceImpl.java
@@ -13,6 +13,8 @@ public class OrderServiceImpl implements OrderService {
     // 정액 할인 정책
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     // 정률 할인 정책
+    //TODO  : OrderServiceImpl은 현재 DiscountPolicy라는 추상(인터페이스) 뿐만 아니라 RateDiscountPolicy에도 의존하고 있다.
+    //FIXME : OCP를 위반하고 있으므로, 인터페이스에만 의존하도록 의존 관계를 변경하면 된다!
     private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
