ddadc7d218b9ab651dd859dd0ea00f466f6c20cb
Supreme-YS <dudtjr1225@gmail.com>
2022-01-29 19:21:41 +0900

    Implement Product Delete Test Code

--- backend/src/test/java/com/kmong/backend/modules/product/ProductServiceTest.java
+++ backend/src/test/java/com/kmong/backend/modules/product/ProductServiceTest.java
@@ -42,4 +42,27 @@ class ProductServiceTest {
         Product productById = productService.findProductById(newProduct.getId());
         Assertions.assertThat(newProduct.getProductName()).isEqualTo(productById.getProductName());
     }
+
+    @Test
+    void deleteProductById() {
+        //given
+        Account account = new Account();
+        account.setId(1L);
+        account.setEmail("dudtjr1225@gmail.com");
+        account.setPassword("testpw");
+        accountService.signUp(account);
+
+        //when
+        Product product = new Product();
+        product.setId(1L);
+        product.setProductName("첫 번째 상품");
+        product.setProductPrice("1000");
+        product.setAccount(account);
+        Product newProduct = productService.addProduct(product);
+
+        //then
+        productService.deleteProductById(newProduct.getId());
+        Optional<Product> id = productRepository.findById(1L);
+        Assertions.assertThat(id).isEmpty();
+    }
 }
