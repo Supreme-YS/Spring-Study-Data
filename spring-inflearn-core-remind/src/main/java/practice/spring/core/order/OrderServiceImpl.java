a78a7f230c6a85869d73d0768df52420014af920
Supreme-YS <dudtjr1225@gmail.com>
2022-01-30 16:33:28 +0900

    Update

--- spring-inflearn-core-remind/src/main/java/practice/spring/core/order/OrderServiceImpl.java
+++ spring-inflearn-core-remind/src/main/java/practice/spring/core/order/OrderServiceImpl.java
@@ -10,12 +10,17 @@ import practice.spring.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //TODO  : OrderServiceImpl은 현재 DiscountPolicy라는 추상(인터페이스) 뿐만 아니라 RateDiscountPolicy에도 의존하고 있다.
+    //FIXME : OCP를 위반하고 있으므로, 인터페이스에만 의존하도록 의존 관계를 변경하면 된다!
+
     // 정액 할인 정책
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     // 정률 할인 정책
-    //TODO  : OrderServiceImpl은 현재 DiscountPolicy라는 추상(인터페이스) 뿐만 아니라 RateDiscountPolicy에도 의존하고 있다.
-    //FIXME : OCP를 위반하고 있으므로, 인터페이스에만 의존하도록 의존 관계를 변경하면 된다!
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    //TODO : 이후 AppConfig로 전체 동작 방식을 구성한다.
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
