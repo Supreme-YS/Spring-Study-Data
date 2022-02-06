976820efebd4e124b27f6ec8e0fb487665a29f6b
Supreme-YS <dudtjr1225@gmail.com>
2022-02-06 17:50:29 +0900

    Refactoring AppConfig

--- spring-inflearn-core-remind/src/main/java/practice/spring/core/AppConfig.java
+++ spring-inflearn-core-remind/src/main/java/practice/spring/core/AppConfig.java
@@ -1,6 +1,8 @@
 package practice.spring.core;
 
+import practice.spring.core.discount.DiscountPolicy;
 import practice.spring.core.discount.FixDiscountPolicy;
+import practice.spring.core.member.MemberRepository;
 import practice.spring.core.member.MemberService;
 import practice.spring.core.member.MemberServiceImpl;
 import practice.spring.core.member.MemoryMemberRepository;
@@ -10,12 +12,21 @@ import practice.spring.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy()
+        );
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
